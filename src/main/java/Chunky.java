24c6fdfb063459a72a7be30c7382a490be211c15
zhonggruii <leezhongrui1@gmail.com>
2025-08-20 00:49:02 +0800

    allow bot to echo commands

--- src/main/java/Chunky.java
+++ src/main/java/Chunky.java
@@ -1,5 +1,7 @@
 package main.java;
 
+import java.util.Scanner;
+
 public class Chunky {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -13,5 +15,19 @@ public class Chunky {
         System.out.println("----------------------------\n");
         System.out.println("Bye! Hope to see you again soon!");
         System.out.println("----------------------------\n");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Say hi to Chunky :)\n");
+
+        String txt = sc.nextLine();
+        while (!txt.equals("bye")) {
+            System.out.println("----------------------------\n");
+            System.out.println(txt);
+            System.out.println("----------------------------\n");
+            txt = sc.nextLine();
+        }
+        System.out.println("----------------------------\n");
+        System.out.println(txt + " See ya!");
+        System.out.println("----------------------------\n");
+        sc.close();
     }
 }
